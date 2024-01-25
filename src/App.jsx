19079817d1d641923098eae84e13fb9f06b5c511
Ijal Fauzi <ijaliffauzi@gmail.com>
2024-01-25 14:44:30 +0700

    Integrate Tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './styles/main.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +29,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <div className='bg-blue-500 text-white p-4'>
+        Hello, TailwindCSS
+      </div>
     </>
   )
 }
