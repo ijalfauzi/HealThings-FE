d3aee7f03337c6aec77daf1af1dc6123628e8f7f
Ijal Fauzi <ijaliffauzi@gmail.com>
2024-01-25 15:13:52 +0700

    Setup homepage

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Homepage() {
+  return (
+    <div>
+      <h1>Welcome to Our Website</h1>
+      <p>This is the homepage of our website.</p>
+    </div>
+  );
+}
+
+export default Homepage;
